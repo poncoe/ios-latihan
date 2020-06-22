fcff37b80be429cd8fd7585c3960a15dcf11472a
poncoe <poncoe4mac@gmail.com>
2020-06-22 16:04:54 +0700

    Initial Commit

---
+++ DataDiri/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  DataDiri
+//
+//  Created by Poncoe on 22/06/20.
+//  Copyright © 2020 Berandalan Jenius. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
