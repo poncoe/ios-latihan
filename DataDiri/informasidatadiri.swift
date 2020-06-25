c8fd92b73dcf99a25c31e5bdf54cea5a2de64809
poncoe <poncoe4mac@gmail.com>
2020-06-25 20:35:04 +0700

    informasi data diri

---
+++ DataDiri/informasidatadiri.swift
@@ -0,0 +1,29 @@
+//
+//  informasidatadiri.swift
+//  DataDiri
+//
+//  Created by Poncoe on 22/06/20.
+//  Copyright © 2020 Berandalan Jenius. All rights reserved.
+//
+
+import Foundation
+
+// Input Data Diri (HARDCODED)
+
+//print("Selamat Datang di Informasi Data Diri Poncoe")
+//let firstName = "Arief", lastName = "Wardhana"
+//let nickName = "Poncoe"
+//let fullName = firstName + " " + lastName
+//let address = "Bekasi - Bandung"
+//let job = "iOS Developer"
+//let age = 0b10101
+
+//print("")
+
+// Output Data Diri (HARDCODED)
+
+//print("Nama Anda Adalah \(fullName)")
+//print("Biasanya di kenal dengan nama \(nickName)")
+//print("\(nickName) adalah seorang \(job)")
+//print("Saat ini ia berumur \(age) dan bertempat tinggal di \(address)")
+//print("")
