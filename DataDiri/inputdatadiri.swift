f98e10bfd927ef01a3f4be9b0ce6836628ae223a
poncoe <poncoe4mac@gmail.com>
2020-06-25 20:35:11 +0700

    input data diri

---
+++ DataDiri/inputdatadiri.swift
@@ -0,0 +1,27 @@
+//
+//  inputdatadiri.swift
+//  DataDiri
+//
+//  Created by Poncoe on 22/06/20.
+//  Copyright © 2020 Berandalan Jenius. All rights reserved.
+//
+
+import Foundation
+
+// Input Data Diri
+
+//print("Selamat Datang di Form Data Diri")
+//
+//print("Masukan Nama Anda : "); let fullName = readLine()!
+//print("Masukan Nama Panggilan : "); let nickName = readLine()!
+//print("Masukan Umur Anda : "); let age = readLine()!
+//print("Masukan Alamat Anda : "); let address = readLine()!
+//print("Masukan Perkerjaan Anda : "); let job = readLine()!
+//
+//// Output Data Diri
+//
+//print("Nama Anda Adalah \(fullName)")
+//print("Biasanya di kenal dengan nama \(nickName)")
+//print("\(nickName) adalah seorang \(job)")
+//print("Umurnya \(age)")
+//print("Tempat Tinggalnya di \(address)")
