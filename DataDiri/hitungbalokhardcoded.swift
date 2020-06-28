c280bde57e78b9f87f6635ff24e0362416985f57
poncoe <poncoe4mac@gmail.com>
2020-06-29 01:17:20 +0700

    hitung balok hardcoded

---
+++ DataDiri/hitungbalokhardcoded.swift
@@ -0,0 +1,38 @@
+//
+//  main.swift
+//  Menghitung Balok
+//
+//  Created by Poncoe on 29/06/20.
+//  Copyright © 2020 Berandalan Jenius. All rights reserved.
+//
+
+import Foundation
+
+//print("Selamat Datang di Hitung Balok")
+//
+//// input Balok Hardcoded
+//let panjang = 4.0
+//let lebar = 2.0
+//let tinggi = 3.0
+//
+//
+//// proses dalam penghitungan balok
+//let volume = panjang * tinggi * lebar
+//let luasPermukaan = 2 * ((lebar * panjang) + (lebar * tinggi) + (tinggi * panjang))
+//let keliling = 4 * (lebar + panjang + tinggi)
+//
+//
+//print("")
+//
+//// output input balok
+//print("Anda memiliki sebuah balok dengan:")
+//print("")
+//
+//print("Lebarnya adalah \(lebar) cm")
+//print("Tingginya adalah \(tinggi) cm")
+//print("Panjangnya adalah \(panjang) cm")
+//print("Volemenya adalah \(volume) cm3")
+//print("Luas permukaannya adalah \(luasPermukaan) cm2")
+//print("Kelilingnya adalah \(keliling) cm")
+//
+//print("")
