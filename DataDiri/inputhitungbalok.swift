97f89f0753db1f780ccbcf58dd820443cbdc770c
poncoe <poncoe4mac@gmail.com>
2020-06-29 01:17:29 +0700

    input balok

---
+++ DataDiri/inputhitungbalok.swift
@@ -0,0 +1,43 @@
+//
+//  main.swift
+//  Menghitung Balok
+//
+//  Created by Poncoe on 29/06/20.
+//  Copyright © 2020 Berandalan Jenius. All rights reserved.
+//
+
+import Foundation
+
+//print("Selamat Datang di Hitung Balok")
+//
+//// input Balok
+//print("Masukkanlah lebar balok:"); let inputLebar = readLine()
+//print("Masukkanlah tinggi balok:"); let inputTinggi = readLine()
+//print("Masukkanlah panjang balok:"); let inputPanjang = readLine()
+//
+//
+//// kondisi jika benar (mengubah dari string ke double)
+//if let panjang = Double(inputPanjang ?? "0"),
+//    let tinggi = Double(inputTinggi ?? "0"),
+//    let lebar = Double(inputLebar ?? "0"){
+//    // proses penghitungan balok
+//    let volume = panjang * tinggi * lebar
+//    let luasPermukaan = 2 * ((lebar * panjang) + (lebar * tinggi) + (tinggi * panjang))
+//    let keliling = 4 * (lebar + panjang + tinggi)
+//    print("")
+//    // Output Hitung Balok
+//    print("Anda memiliki sebuah balok dengan:")
+//    print("")
+//    print("Lebarnya adalah \(lebar) cm")
+//    print("Tingginya adalah \(tinggi) cm")
+//    print("Panjangnya adalah \(panjang) cm")
+//    print("Volemenya adalah \(volume) cm3")
+//    print("Luas permukaannya adalah \(luasPermukaan) cm2")
+//    print("Kelilingnya adalah \(keliling) cm")
+//    print("")
+//} else {
+//    // kondisi jika input tidak valid
+//    print("")
+//    print("Input tidak valid")
+//    print("")
+//}
