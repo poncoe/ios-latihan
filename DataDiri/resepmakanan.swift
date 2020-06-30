4958f27c4dfbb241d9d59e8cee9722bd90a74074
poncoe <poncoe4mac@gmail.com>
2020-07-01 00:32:01 +0700

    resep makanan

---
+++ DataDiri/resepmakanan.swift
@@ -0,0 +1,39 @@
+//
+//  resepmakanan.swift
+//  BelajarXCode
+//
+//  Created by Poncoe on 01/07/20.
+//  Copyright © 2020 Berandalan Jenius. All rights reserved.
+//
+
+import Foundation
+
+//print("Selamat Datang di Resep Makanan")
+//print("Mari membuat makanan!")
+//print("")
+//print("Apa yang ingin Anda buat?");let makanan = readLine() ?? ""
+//print("Masukkan jumlah item yang ingin Anda masukkan", terminator: ": "); let newItem = readLine() ?? "0"
+//print("")
+//
+//if let totalItem : Int = Int(newItem){
+//    var resep = [String]()
+//    for index in 1...totalItem {
+//        print("Masukan item ke \(index)", terminator: ": "); let newItem = String(readLine() ?? "")
+//        resep.append(newItem)
+//    }
+//
+//    print("")
+//
+//    // untuk input waktu dikasih keterangan detik, menit / jam. misal 2 menit
+//    print("Berapa lama proses memasak dilakukan?");let waktu = readLine() ?? ""
+//    print("")
+//    resep.sort()
+//    print("Anda akan membuat \(makanan) selama \(waktu) dengan resep:")
+//    for (index, value) in resep.enumerated() {
+//        print("\(index + 1). \(value)")
+//    }
+//
+//} else {
+//    print("Input yang anda masukan tidak valid")
+//}
+//print("")
